f607767293ebb344316b3f21ecbdc8b13788cc65
Elton dos Santos <cooperton4@gmail.com>
2017-09-05 12:51:37 -0300

    Aula 04 - Spring MVC I

--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -16,8 +18,11 @@ public class ProdutoDAO {
 	private EntityManager manager;
 
 	public void gravar(Produto produto) {
-
 		manager.persist(produto);
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("SELECT p FROM Produto p", Produto.class).getResultList();
 	}
 
 }
